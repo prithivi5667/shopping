a28e931b8605af713d444d12c4779023f66c2d96
prithivi5667 <prithivi5667@gmail.com>
2021-12-16 20:54:52 +0530

    my first commit

--- frontend/src/pages/PaymentMethod.js
+++ frontend/src/pages/PaymentMethod.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { savePaymentMethod } from '../actions/CartAction';
+import { savePaymentMethod } from '../actions/cartAction';
 import CheckoutSteps from '../components/CheckoutSteps'
 import "../styles/PaymentMethod.css"
 
